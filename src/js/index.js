b4f759128a5bb64ae4261e59430cf3434c06492f
Justin <info@justinlmartin.com>
2021-07-02 14:47:49 -0700

    add div click function

--- src/js/index.js
+++ src/js/index.js
@@ -33,9 +33,41 @@ function setIconTextSize() {
     }
 }
 
+function handleClick(event) {
+    let id = event.target.id
+    console.log(event.target.id)
+    switch(id) {
+        case 'instagram':
+            window.open('https://www.instagram.com/jugglingtallguy/')
+            break
+        case 'venmo':
+            window.open('https://www.venmo.com/u/jmartin432')
+            break
+        case 'email':
+            window.open('mailto:info@justinlmartin.com')
+            break
+        case 'coding-portfolio':
+            window.open('https://coding-portfolio.justinlmartin.com')
+            break
+        case 'github':
+            window.open('https://github.com/jmartin432')
+            break
+        default:
+            return
+}
+
+
+
+}
+
 window.onload = (event) => {
     isMobile = detectMobile()
     setIconTextSize()
+    let buttons = document.querySelectorAll('.link-container')
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        button.addEventListener('click', handleClick)
+    }
 }
 
 window.onresize = (event) => {
