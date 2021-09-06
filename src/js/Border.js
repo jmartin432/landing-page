5f7b3af3eb4fb305ac13d21d8f73b516ceeb7dbd
Justin <info@justinlmartin.com>
2021-09-06 16:30:24 -0700

    updated object constructors

--- src/js/Border.js
+++ src/js/Border.js
@@ -28,65 +28,65 @@ export function Border(id, size, radius, noiseStep) {
     this.containerDiv.addEventListener('touchleave', () => {
         this.noiseStep = .005
     })
-}
 
-Border.prototype.createPoints = function() {
-    let points = []
-    for (let i = 0; i < this.size; i++) {
-        let theta = i * this.angleStep;
+    this.createPoints = function() {
+        let points = []
+        for (let i = 0; i < this.size; i++) {
+            let theta = i * this.angleStep;
 
-        let anchor = new AnchorPoint(theta, this.radius)
-        let controlPoint = new ControlPoint(theta + ((1 / 3) * this.angleStep), this.radius)
-        let controlPointReflected = new ReflectedControlPoint(anchor, controlPoint)
+            let anchor = new AnchorPoint(theta, this.radius)
+            let controlPoint = new ControlPoint(theta + ((1 / 3) * this.angleStep), this.radius)
+            let controlPointReflected = new ReflectedControlPoint(anchor, controlPoint)
 
-        points.push(controlPointReflected, anchor, controlPoint);
+            points.push(controlPointReflected, anchor, controlPoint);
+        }
+        this.points = points
+        return this
     }
-    this.points = points
-    return this
-}
 
-Border.prototype.updatePoints = function() {
-    for (let i = 0; i < this.size; i++) {
-        const reflectedControlPoint = this.points[3 * i]
-        const anchor = this.points[3 * i + 1]
-        const controlPoint = this.points[3 * i + 2];
+    this.updatePoints = function() {
+        for (let i = 0; i < this.size; i++) {
+            const reflectedControlPoint = this.points[3 * i]
+            const anchor = this.points[3 * i + 1]
+            const controlPoint = this.points[3 * i + 2];
 
-        controlPoint.updateNoiseOffsets(this.noiseStep).updateRadiusDelta().updateXY()
-        reflectedControlPoint.updateXY(anchor, controlPoint)
+            controlPoint.updateNoiseOffsets(this.noiseStep).updateRadiusDelta().updateXY()
+            reflectedControlPoint.updateXY(anchor, controlPoint)
+        }
+        return this
     }
-    return this
-}
 
-Border.prototype.makePath = function() {
-    let data = 'M ' + this.points[1].x + ' ' + this.points[1].y;
-    for (let i = 2; i < this.points.length - 2; i += 3) {
-        data = data + ' C ' + this.points[i].x + ' ' + this.points[i].y + ' ' +
-            this.points[i + 1].x + ' ' + this.points[i + 1].y + ' ' +
-            this.points[i + 2].x + ' ' + this.points[i + 2].y
+    this.makePath = function() {
+        let data = 'M ' + this.points[1].x + ' ' + this.points[1].y;
+        for (let i = 2; i < this.points.length - 2; i += 3) {
+            data = data + ' C ' + this.points[i].x + ' ' + this.points[i].y + ' ' +
+                this.points[i + 1].x + ' ' + this.points[i + 1].y + ' ' +
+                this.points[i + 2].x + ' ' + this.points[i + 2].y
+        }
+        // here we are adding the last control point  which is a reflection of the first control point and then
+        // adding the last anchor point to close the loop
+        data = data + ' C ' + this.points[this.points.length - 1].x + ' ' + this.points[this.points.length - 1].y + ' ' +
+            this.points[0].x + ' ' + this.points[0].y + ' ' +
+            this.points[1].x + ' ' + this.points[1].y
+        this.path = data
+        return this
     }
-    // here we are adding the last control point  which is a reflection of the first control point and then
-    // adding the last anchor point to close the loop
-    data = data + ' C ' + this.points[this.points.length - 1].x + ' ' + this.points[this.points.length - 1].y + ' ' +
-        this.points[0].x + ' ' + this.points[0].y + ' ' +
-        this.points[1].x + ' ' + this.points[1].y
-    this.path = data
-    return this
-}
 
-Border.prototype.setPathElementData = function() {
-    this.pathElement.setAttribute('d', this.path)
-    return this
-}
+    this.setPathElementData = function() {
+        this.pathElement.setAttribute('d', this.path)
+        return this
+    }
 
-Border.prototype.setBorderGlowColor = function() {
-    this.borderGlowElement.setAttribute('stroke', helpers.makeColor(this.hue, 50, 65))
-}
+    this.setBorderGlowColor = function() {
+        this.borderGlowElement.setAttribute('stroke', helpers.makeColor(this.hue, 50, 65))
+    }
 
-Border.prototype.setBorderColor = function() {
-    this.borderElement.setAttribute('stroke', helpers.makeColor(this.hue,100, 50),)
-}
+    this.setBorderColor = function() {
+        this.borderElement.setAttribute('stroke', helpers.makeColor(this.hue,100, 50),)
+    }
 
-Border.prototype.setBackGroundColorClipPath = function() {
-    if (this.id === 'header-image') return
-    this.containerDiv.setAttribute('style', 'clip-path: url(#' + this.id + '-clip-path);background-color: ' + helpers.makeColor(this.hue, 50, 75))
+    this.setBackGroundColorClipPath = function() {
+        if (this.id === 'header-image') return
+        this.containerDiv.setAttribute('style', 'clip-path: url(#' + this.id + '-clip-path);background-color: ' + helpers.makeColor(this.hue, 50, 75))
+    }
 }
--- src/js/Points.js
+++ src/js/Points.js
@@ -18,33 +18,33 @@ export function ControlPoint(angle, radius) {
     this.radiusDelta = .1 * helpers.noise(this.noiseOffsetX, this.noiseOffsetY)
     this.x = Math.cos(angle) * (radius + this.radiusDelta)
     this.y = Math.sin(angle) * (radius + this.radiusDelta)
-}
 
-ControlPoint.prototype.updateNoiseOffsets = function(delta) {
-    this.noiseOffsetX += delta;
-    this.noiseOffsetY += delta;
-    return this
-}
+    this.updateNoiseOffsets = function (delta) {
+        this.noiseOffsetX += delta;
+        this.noiseOffsetY += delta;
+        return this
+    }
 
-ControlPoint.prototype.updateRadiusDelta = function(){
-    this.radiusDelta = .1 * helpers.noise(this.noiseOffsetX, this.noiseOffsetY)
-    return this
-}
+    this.updateRadiusDelta = function(){
+        this.radiusDelta = .1 * helpers.noise(this.noiseOffsetX, this.noiseOffsetY)
+        return this
+    }
 
-ControlPoint.prototype.updateXY = function() {
-    this.x = Math.cos(this.angle) * (this.radius + this.radiusDelta)
-    this.y = Math.sin(this.angle) * (this.radius + this.radiusDelta)
-    return this
+    this.updateXY = function() {
+        this.x = Math.cos(this.angle) * (this.radius + this.radiusDelta)
+        this.y = Math.sin(this.angle) * (this.radius + this.radiusDelta)
+        return this
+    }
 }
 
 export function ReflectedControlPoint (anchor, control) {
     this.type = 'reflected-control'
     this.x = 2 * anchor.x - control.x
     this.y = 2 * anchor.y - control.y
-}
 
-ReflectedControlPoint.prototype.updateXY = function(anchor, control) {
-    this.x = 2 * anchor.x - control.x
-    this.y = 2 * anchor.y - control.y
-    return this
+    this.updateXY = function(anchor, control) {
+        this.x = 2 * anchor.x - control.x
+        this.y = 2 * anchor.y - control.y
+        return this
+    }
 }
