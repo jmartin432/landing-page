8610a94128dfa2f992632b3028c3502208cc80f8
Justin <info@justinlmartin.com>
2022-01-14 09:22:44 -0600

    updating links, whatsapp and blog

--- src/js/extraLinkData.js
+++ src/js/extraLinkData.js
@@ -1,26 +1,5 @@
 export const links = [
     {
-        id: 'blog',
-        text: 'Check Out My Blog',
-        altText: 'Check Out My Blog',
-        link: 'blog.justinlmartin.com',
-        mobileLink: 'blog.justinlmartin.com'
-    },
-    {
-        id: 'whatsapp',
-        text: 'Contact Me on WhatsApp',
-        altText: 'Contact Me on WhatsApp',
-        link: 'https://wa.me/14135193798',
-        mobileLink: 'https://wa.me/14135193798'
-    },
-    {
-        id: 'instagram',
-        text: 'See My Instagram',
-        altText: 'See My Instagram',
-        link: 'https://www.instagram.com/jugglingtallguy/',
-        mobileLink: 'instagram://user?username=jugglingtallguy'
-    },
-    {
         id: 'email',
         text: 'Send Me an E-mail',
         altText: 'Send Me an E-mail',
@@ -28,11 +7,25 @@ export const links = [
         mobileLink: 'mailto:info@justinlmartin.com'
     },
     {
+        id: 'postcard',
+        text: 'Send Me a Postcard',
+        altText: 'PO Box 11967, Portland OR, 97211',
+        link: '',
+        mobileLink: ''
+    },
+    {
         id: 'venmo',
         text: 'Send Me Money on Venmo',
         altText: 'Send Me Money on Venmo',
         link: 'https://www.venmo.com/u/justinlmartin',
         mobileLink: 'https://www.venmo.com/justinlmartin?txn=pay'
+    },
+    {
+        id: 'coding-portfolio',
+        text: 'Coding Portfolio',
+        altText: 'Coding Portfolio',
+        link: 'https://coding-portfolio.justinlmartin.com',
+        mobileLink: 'https://coding-portfolio.justinlmartin.com'
     },
     {
         id: 'github',
